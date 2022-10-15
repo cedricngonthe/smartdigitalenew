150b893979d823799b8735b03d9571b48568c7d3
BleriotWafo <gabinwafo@yahoo.com>
2022-10-15 13:13:15 +0200

    "création Navbar"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './Components/Navbar'
+import './Components/Hero'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Smart digital
-        </a>
-      </header>
+      <Navbar />
+      <Hero/>
+
+
+
     </div>
   );
 }
